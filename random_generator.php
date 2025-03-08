36cc5c26f8b6bff1bbc93c28c7722f8f4f4bd4be
Ahmet <ahmetbarut588@gmail.com>
2025-03-08 20:52:34 +0300

    rastgele şehir ve tarih üretimi eklendi

--- random_generator.php
+++ random_generator.php
@@ -20,4 +20,17 @@ $colors = ['kırmızı', 'mavi', 'yeşil', 'sarı', 'mor'];
 $randomColor = $colors[array_rand($colors)];
 echo "Rastgele Renk: " . $randomColor;
 
+// Rastgele şehir seçimi için dizi
+$cities = ['İstanbul', 'Ankara', 'İzmir', 'Bursa', 'Antalya', 'Eskişehir', 'Trabzon'];
+$randomCity = $cities[array_rand($cities)];
+
+// Rastgele tarih üretimi
+$startDate = strtotime("2020-01-01");
+$endDate = strtotime("2023-12-31");
+$randomTimestamp = mt_rand($startDate, $endDate);
+$randomDate = date("Y-m-d", $randomTimestamp);
+
+echo "\nRastgele Şehir: " . $randomCity;
+echo "\nRastgele Tarih: " . $randomDate;
+
 ?>
